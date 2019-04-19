eddfd222c04e115041a653c797cc8cb73c2365f6
Rishi <ambwanirishi@gmail.com>
2019-04-19 11:47:23 +0530

    server

--- login_registration/wsgi.py
+++ login_registration/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login_registration.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login_registration.settings.production")
 
 application = get_wsgi_application()
